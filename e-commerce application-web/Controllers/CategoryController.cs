5229d084c20ac850b9b944aa4ae56313f3a1c091
Joey <joelnl74@gmail.com>
2023-07-29 03:01:43 +0200

    Init

---
+++ e-commerce application-web/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using e_commerce_application_web.Data;
+using e_commerce_application_web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce_application_web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _dbContext.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
---
+++ e-commerce application-web/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using e_commerce_application_web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce_application_web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, DisplayOrder = 0, Name = "Category A"},
+                new Category { Id = 2, DisplayOrder = 1, Name = "Category B" },
+                new Category { Id = 3, DisplayOrder = 2, Name = "Category C" }
+                );
+        }
+    }
+
+}
